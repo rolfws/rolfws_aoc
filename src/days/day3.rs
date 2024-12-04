fe63edd5b6abfdf2cbe3dadcd6f5931b96be46d3
rolfws <rolfscheffers@live.nl>
2024-12-04 15:01:34 +0100

    Better match statements.

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -8,19 +8,19 @@ fn three_u8_to_u32(in1: u8, in2: u8, in3: u8) -> u32 {
     (in1 - b'0') as u32 * 100 + (in2 - b'0') as u32 * 10 + (in3 - b'0') as u32
 }
 
-#[allow(clippy::missing_safety_doc)]
 unsafe fn part_1_wrap(inp: &str) -> u32 {
     let mut acc = 0;
     let mut i: usize = 0;
     let mut left_reg: u32;
     let mut right_reg: u32;
     // Max we read is from i..i+12, so we extend the &[u8] with 4 elements that will be faulty
+    // 12 is the max length of mul(xyz,abc)
     let inp_s = [inp, "...."].concat();
     let inp = inp_s.as_bytes();
     let ptr = inp.as_ptr();
-    
-    while i < inp.len() - 12 {
-        // println!("{:?}", std::str::from_utf8(&inp[i..i+4]));
+
+    while i < inp.len() - 11 {
+        // Slices len is atleast 12 elements so all 3 sets of 4 reads are all safe.
         match [
             ptr.add(i).read(),
             ptr.add(i + 1).read(),
@@ -28,22 +28,25 @@ unsafe fn part_1_wrap(inp: &str) -> u32 {
             ptr.add(i + 3).read(),
         ] {
             [b'm', b'u', b'l', b'('] => i += 4,
-            [b'm', b'u', b'l', _] => {
-                i += 3;
+            [_, b'm', b'u', b'l'] => {
+                i += 1;
                 continue;
             }
-            [b'm', b'u', _, _] => {
+            [_, _, b'm', b'u'] => {
                 i += 2;
                 continue;
             }
+            [_, _, _, b'm'] => {
+                i += 3;
+                continue;
+            }
             _ => {
-                i += 1;
+                i += 4;
                 continue;
             } // IF slice is not 4 elements we do not go into this by while.
         }
 
         // Yay we got mul(
-        // println!("{:?}", std::str::from_utf8(&inp[i..i+4]));
         match [
             ptr.add(i).read(),
             ptr.add(i + 1).read(),
@@ -54,24 +57,33 @@ unsafe fn part_1_wrap(inp: &str) -> u32 {
                 left_reg = three_u8_to_u32(inp[i], inp[i + 1], inp[i + 2]);
                 i += 4
             }
-            [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9', _] => {
-                i += 3;
-                continue;
-            }
             [b'0'..=b'9', b'0'..=b'9', b',', _] => {
                 left_reg = two_u8_to_u32(inp[i], inp[i + 1]);
                 i += 3
-            }
-            [b'0'..=b'9', b'0'..=b'9', _, _] => {
-                i += 2;
-                continue;
             }
             [b'0'..=b'9', b',', _, _] => {
                 left_reg = (inp[i] - b'0') as u32;
                 i += 2
             }
-            _ => {
+
+            // No numbers with comma, but we read any way so we check for mul(
+            [b'm', b'u', b'l', b'('] => {
+                continue;
+            } // This could be done a little more efficient
+            [_, b'm', b'u', b'l'] => {
                 i += 1;
+                continue;
+            }
+            [_, _, b'm', b'u'] => {
+                i += 2;
+                continue;
+            }
+            [_, _, _, b'm'] => {
+                i += 3;
+                continue;
+            }
+            _ => {
+                i += 4;
                 continue;
             }
         }
@@ -85,25 +97,33 @@ unsafe fn part_1_wrap(inp: &str) -> u32 {
             [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9', b')'] => {
                 right_reg = three_u8_to_u32(inp[i], inp[i + 1], inp[i + 2]);
                 i += 4
-            }
-            [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9', _] => {
-                i += 3;
-                continue;
             }
             [b'0'..=b'9', b'0'..=b'9', b')', _] => {
                 right_reg = two_u8_to_u32(inp[i], inp[i + 1]);
                 i += 3
             }
-            [b'0'..=b'9', b'0'..=b'9', _, _] => {
-                i += 2;
-                continue;
-            }
             [b'0'..=b'9', b')', _, _] => {
                 right_reg = (inp[i] - b'0') as u32;
                 i += 2
             }
-            _ => {
+
+            [b'm', b'u', b'l', b'('] => {
+                continue;
+            } // This could be done a little more efficient, as the first check is now done twice.
+            [_, b'm', b'u', b'l'] => {
                 i += 1;
+                continue;
+            }
+            [_, _, b'm', b'u'] => {
+                i += 2;
+                continue;
+            }
+            [_, _, _, b'm'] => {
+                i += 3;
+                continue;
+            }
+            _ => {
+                i += 4;
                 continue;
             }
         }
@@ -112,95 +132,6 @@ unsafe fn part_1_wrap(inp: &str) -> u32 {
     acc
 }
 
-pub fn part1_safe(inp: &[u8]) -> u32 {
-    let mut acc = 0;
-    let mut i: usize = 0;
-    let mut left_reg: u32;
-    let mut right_reg: u32;
-    while i < inp.len() - 8 {
-        // println!("{:?}", std::str::from_utf8(&inp[i..i+4]));
-        match inp[i..i + 4] {
-            [b'm', b'u', b'l', b'('] => i += 4,
-            [b'm', b'u', b'l', _] => {
-                i += 3;
-                continue;
-            }
-            [b'm', b'u', _, _] => {
-                i += 2;
-                continue;
-            }
-            _ => {
-                i += 1;
-                continue;
-            } // IF slice is not 4 elements we do not go into this by while.
-        }
-
-        // Yay we got mul(
-        // Now we need to check if up to next 3 are digits
-        // Here we still can not go out of bounds as 8 - 4 = 4
-        // println!("{:?}", std::str::from_utf8(&inp[i..i+4]));
-        match inp[i..i + 4] {
-            [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9', b','] => {
-                left_reg = three_u8_to_u32(inp[i], inp[i + 1], inp[i + 2]);
-                i += 4
-            }
-            [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9', _] => {
-                i += 3;
-                continue;
-            }
-            [b'0'..=b'9', b'0'..=b'9', b',', _] => {
-                left_reg = two_u8_to_u32(inp[i], inp[i + 1]);
-                i += 3
-            }
-            [b'0'..=b'9', b'0'..=b'9', _, _] => {
-                i += 2;
-                continue;
-            }
-            [b'0'..=b'9', b',', _, _] => {
-                left_reg = (inp[i] - b'0') as u32;
-                i += 2
-            }
-            _ => {
-                i += 1;
-                continue;
-            }
-        }
-
-        match inp[i..std::cmp::min(i + 4, inp.len() - 1)] {
-            [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9', b')'] => {
-                right_reg = three_u8_to_u32(inp[i], inp[i + 1], inp[i + 2]);
-                i += 4
-            } // Valid 3
-            [b'0'..=b'9', b'0'..=b'9', b')', _] | [b'0'..=b'9', b'0'..=b'9', b')'] => {
-                right_reg = two_u8_to_u32(inp[i], inp[i + 1]);
-                i += 3
-            } // Valid 2
-            [b'0'..=b'9', b')', _, _] | [b'0'..=b'9', b')', _] | [b'0'..=b'9', b')'] => {
-                right_reg = (inp[i] - b'0') as u32;
-                i += 2
-            } // Valid 1
-            [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9', _] => {
-                i += 3;
-                continue;
-            }
-            [b'0'..=b'9', b'0'..=b'9', _, _] => {
-                i += 2;
-                continue;
-            }
-            _ => {
-                i += 1;
-                continue;
-            }
-        }
-        // Yay we got mul(x,
-        // Remains to check for y). Now we can go out of range.
-        acc += left_reg * right_reg
-    }
-    acc
-}
-
-
-#[allow(clippy::missing_safety_doc)]
 unsafe fn part_2_core(inp: &str) -> u32 {
     let mut doing = true;
     let mut acc = 0;
@@ -211,24 +142,31 @@ unsafe fn part_2_core(inp: &str) -> u32 {
     let inp_s = [inp, "...."].concat();
     let inp = inp_s.as_bytes();
     let ptr = inp.as_ptr();
-    
-    while i < inp.len() - 12 {
+
+    while i < inp.len() - 11 {
+        // Len is 12 so this part is safe
         if !doing {
             // Check if the next 4 are do(), else we cont
-            match [ptr.add(i).read(),
-            ptr.add(i + 1).read(),
-            ptr.add(i + 2).read(),
-            ptr.add(i + 3).read()
+            match [
+                ptr.add(i).read(),
+                ptr.add(i + 1).read(),
+                ptr.add(i + 2).read(),
+                ptr.add(i + 3).read(),
             ] {
-                [b'd', b'o', b'(', b')'] => {i+=4; doing=true},
-                [b'd', b'o', b'(', _] => {i+=3},
-                [b'd', b'o', _, _] => {i+=2},
-                _ => {i+=1}
+                [b'd', b'o', b'(', b')'] => {
+                    i += 4;
+                    doing = true
+                }
+                [_, b'd', b'o', b'('] => i += 1,
+                [_, _, b'd', b'o'] => i += 2,
+                [_, _, _, b'd'] => i += 3,
+                _ => i += 4,
             }
             continue;
         }
-        
+
         // Check for mul( 4 or don't() 7
+        // Len is 12 so this part is safe
         match [
             ptr.add(i).read(),
             ptr.add(i + 1).read(),
@@ -238,23 +176,53 @@ unsafe fn part_2_core(inp: &str) -> u32 {
             ptr.add(i + 5).read(),
             ptr.add(i + 6).read(),
         ] {
-            [b'd', b'o', b'n', b'\'', b't', b'(', b')'] => {i += 7; doing=false},
+            [b'd', b'o', b'n', b'\'', b't', b'(', b')'] => {
+                i += 7;
+                doing = false;
+                continue;
+            }
             [b'm', b'u', b'l', b'(', _, _, _] => i += 4,
-            [b'm', b'u', b'l', _, _, _, _] => {
-                i += 3;
-                continue;
-            }
-            [b'm', b'u', _, _, _, _, _] => {
-                i += 2;
-                continue;
-            }
-            _ => {
+            [_, b'm', b'u', b'l', b'(', _, _] => i += 5,
+            [_, _, b'm', b'u', b'l', b'(', _] => i += 6,
+            [_, _, _, b'm', b'u', b'l', b'('] => i += 7,
+
+            [_, b'd', b'o', b'n', b'\'', b't', b'('] => {
                 i += 1;
                 continue;
             }
-        }
 
-        // Yay we got mul(
+            [_, _, b'd', b'o', b'n', b'\'', b't'] => {
+                i += 2;
+                continue;
+            }
+
+            [_, _, _, b'd', b'o', b'n', b'\''] => {
+                i += 3;
+                continue;
+            }
+
+            [_, _, _, _, b'd', b'o', b'n'] | [_, _, _, _, b'm', b'u', b'l'] => {
+                i += 4;
+                continue;
+            }
+
+            [_, _, _, _, _, b'd', b'o'] | [_, _, _, _, _, b'm', b'u'] => {
+                i += 5;
+                continue;
+            }
+
+            [_, _, _, _, _, _, b'd'] | [_, _, _, _, _, _, b'm'] => {
+                i += 6;
+                continue;
+            }
+            _ => {
+                i += 7;
+                continue;
+            }
+        }
+        // From the previous there are atleast 12 - 7 = 5 elements left,
+        // if we reached the end we will hit the '....'. So we wont match and terminate.
+        // This last part is essential for the read after this one.
         match [
             ptr.add(i).read(),
             ptr.add(i + 1).read(),
@@ -265,28 +233,39 @@ unsafe fn part_2_core(inp: &str) -> u32 {
                 left_reg = three_u8_to_u32(inp[i], inp[i + 1], inp[i + 2]);
                 i += 4
             }
-            [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9', _] => {
-                i += 3;
-                continue;
-            }
             [b'0'..=b'9', b'0'..=b'9', b',', _] => {
                 left_reg = two_u8_to_u32(inp[i], inp[i + 1]);
                 i += 3
-            }
-            [b'0'..=b'9', b'0'..=b'9', _, _] => {
-                i += 2;
-                continue;
             }
             [b'0'..=b'9', b',', _, _] => {
                 left_reg = (inp[i] - b'0') as u32;
                 i += 2
             }
-            _ => {
+
+            // No numbers, read any way so we check for m / d (Start of maybe next)
+            // do() is not an interesting match, as we already are doing.
+            [b'm', b'u', b'l', b'('] | [b'd', b'o', b'n', b'\''] => {
+                continue;
+            } // This could be done a little more efficient
+            [_, b'm', b'u', b'l'] | [_, b'd', b'o', b'n'] => {
                 i += 1;
+                continue;
+            }
+            [_, _, b'm', b'u'] | [_, _, b'd', b'o'] => {
+                i += 2;
+                continue;
+            }
+            [_, _, _, b'm'] | [_, _, _, b'd'] => {
+                i += 3;
+                continue;
+            }
+            _ => {
+                i += 4;
                 continue;
             }
         }
 
+        // At this point if there was a . we would have continued, and terminated, so next reads are surely in bound.
         match [
             ptr.add(i).read(),
             ptr.add(i + 1).read(),
@@ -297,24 +276,34 @@ unsafe fn part_2_core(inp: &str) -> u32 {
                 right_reg = three_u8_to_u32(inp[i], inp[i + 1], inp[i + 2]);
                 i += 4
             }
-            [b'0'..=b'9', b'0'..=b'9', b'0'..=b'9', _] => {
-                i += 3;
-                continue;
-            }
             [b'0'..=b'9', b'0'..=b'9', b')', _] => {
                 right_reg = two_u8_to_u32(inp[i], inp[i + 1]);
                 i += 3
-            }
-            [b'0'..=b'9', b'0'..=b'9', _, _] => {
-                i += 2;
-                continue;
             }
             [b'0'..=b'9', b')', _, _] => {
                 right_reg = (inp[i] - b'0') as u32;
                 i += 2
             }
-            _ => {
+
+            // No numbers, read any way so we check for m / d (Start of maybe next)
+            // do() is not an interesting match, as we already are doing.
+            [b'm', b'u', b'l', b'('] | [b'd', b'o', b'n', b'\''] => {
+                continue;
+            } // This could be done a little more efficient
+            [_, b'm', b'u', b'l'] | [_, b'd', b'o', b'n'] => {
                 i += 1;
+                continue;
+            }
+            [_, _, b'm', b'u'] | [_, _, b'd', b'o'] => {
+                i += 2;
+                continue;
+            }
+            [_, _, _, b'm'] | [_, _, _, b'd'] => {
+                i += 3;
+                continue;
+            }
+            _ => {
+                i += 4;
                 continue;
             }
         }
@@ -323,27 +312,16 @@ unsafe fn part_2_core(inp: &str) -> u32 {
     acc
 }
 
-// use regex::Regex;
-
-// #[aoc(day3, part1, regex)]
-// pub fn part1_reg(inp:&str) -> u32 {
-//     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-//     re.captures_iter(inp).map(|c| c.extract()).fold(0u32, |acc, (_, [left, right])| 
-//         acc + left.parse::<u32>().unwrap() * right.parse::<u32>().unwrap()
-//     )
-// }   
-
 #[aoc(day3, part1)]
-pub fn part1(inp:&str) -> u32 {
-    unsafe {part_1_wrap(inp)}
+pub fn part1(inp: &str) -> u32 {
+    unsafe { part_1_wrap(inp) }
 }
-
-#[aoc(day3, part1, safe)]
-pub fn part1_s(inp:&[u8]) -> u32 {
-    part1_safe(inp)
-}
+// #[aoc(day3, part1, safe)]
+// pub fn part1_s(inp:&[u8]) -> u32 {
+//     part1_safe(inp)
+// }
 
 #[aoc(day3, part2)]
-pub fn part2(inp:&str) -> u32 {
-    unsafe {part_2_core(inp)}
+pub fn part2(inp: &str) -> u32 {
+    unsafe { part_2_core(inp) }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,18 @@
 
 use std::time::Instant;
 fn main() {
-    // let inp = aoc24::load_day_bytes(3, false);
-    // let runs = 100000;
-    // // let t = Instant::now();
-    // // for _ in 0..runs {
-    // //     let r = unsafe{aoc24::day3::part1(&inp)};
-    // // }
-    // // println!("{:?}", t.elapsed() / runs);
+    let inp = aoc24::load_day(3, false);
+    let runs = 100_000;
+    let t = Instant::now();
+    for _ in 0..runs {
+        let r = aoc24::day3::part1(&inp);
+    }
+    println!("{:?}", t.elapsed() / runs);
 
-    // let t = Instant::now();
-    // for _ in 0..runs {
-    //     aoc24::day3::part2(&inp);
-    // }
-    // println!("{:?}", t.elapsed() / runs);
-    // let r = aoc24::day3::part2(&inp);
-    let inp = aoc24::load_day(2, false);
-    let r = aoc24::day2::part1(&inp);
-    println!("{r}");
+    // let inp = aoc24::load_day(3, true);
+    // let r = aoc24::day3::part1(&inp);
+    // println!("{r}");
 
-    let r = aoc24::day2::part2(&inp);
-    println!("{r}");
+    // let r = aoc24::day2::part2(&inp);
+    // println!("{r}");
 }
