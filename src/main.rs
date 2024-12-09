63b0841e6c8b9051c63b366c1dc84644757a9729
rolfws <rolfscheffers@live.nl>
2024-12-09 16:01:16 +0100

    day 9 and little opt.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@
 
 use std::time::Instant;
 fn main() {
-    // let inp = aoc24::load_day(8, false);
-    // let runs = 1000000;
-    // let t = Instant::now();
-    // for _ in 0..runs {
-    //     let r = aoc24::day8::part2(&inp);
-    // }
-    // println!("{:?}", t.elapsed() / runs);
+    let inp = aoc24::load_day(9, false);
+    let runs = 10000;
+    let t = Instant::now();
+    for _ in 0..runs {
+        let r = aoc24::day9::part2(&inp);
+    }
+    println!("{:?}", t.elapsed() / runs);
 
-    let inp = aoc24::load_day(8, true);
-    let r = aoc24::day8::part2(&inp);
-    println!("{r}");
+    // let inp = aoc24::load_day(9, false);
+    
+    // println!("{r}");
 
     // let inp = aoc24::load_day(4, false);
     // let r = aoc24::day4::part1(&inp);
